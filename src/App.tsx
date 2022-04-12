75f364234c0e1f96ff3c7cebb7aedb0b65128824
Anselem <anselemodims@gmail.com>
2022-04-12 22:55:19 +0100

    Feat: add type interface for the people array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+interface PeopleState{
+  name: string
+  age: number
+  imageUrl: string
+  note: string
+}
+
 function App() {
+  const [people, setPeople] = useState<PeopleState | null>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>People invited to my party</p>
     </div>
   );
 }
